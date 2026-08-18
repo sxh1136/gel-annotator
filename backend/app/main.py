80217597176da1a6cb8d73f735696986260dd89c
sxh1136 <siufungstanleyho@gmail.com>
2026-08-18 17:40:27 +0800

    github pages

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,10 @@ app = FastAPI(title="Gel Annotator API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sxh1136.github.io",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
